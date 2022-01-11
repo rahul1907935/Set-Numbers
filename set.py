573c4aa0901ffebef5bab13044efecf4d2ef7dd0
rahul1907935 <83805686+rahul1907935@users.noreply.github.com>
2022-01-11 19:30:23 +0530

    Set Numbers
    This file has been printed successfully

---
+++ set.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 19:27:06 2022
+
+@author: HP
+"""
+
+
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+
+print("Union of E and N is",E | N)
+
+print("Intersection of E and N is",E & N)
+
+print("Difference of E and N is",E - N)
+
+print("Symmetric difference of E and N is",E ^ N)
